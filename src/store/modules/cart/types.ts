5bcc72b46777f9240e7d3d9491afd0b3e1ec1079
Guga Villar <grsv.21@gmail.com>
2021-11-05 10:58:17 -0300

    Criação do reducer cart

---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,14 @@
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+}
+
+interface ICartItem {
+  product: Product;
+  quantity: number;
+}
+
+export interface ICartState {
+  items: ICartItem[];
+}
