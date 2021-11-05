e3961637faaad6dc8a6f9b1b3ac50a2ffd9ff54a
Guga Villar <grsv.21@gmail.com>
2021-11-05 15:01:02 -0300

    Criação da funcão producer do immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,5 +1,6 @@
 import { Reducer } from "redux";
 import { ICartState } from "./types";
+import produce from 'immer';
 
 const INITIAL_STATE: ICartState = {
   items: []
@@ -7,22 +8,19 @@ const INITIAL_STATE: ICartState = {
 
 
 export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1
-          }
-        ]
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+        draft.items.push({
+          product,
+          quantity: 1
+        });
+        break;
+      }
+      default: {
+        return draft;
       }
     }
-    default: {
-      return state;
-    }
-  }
+  });
 }
