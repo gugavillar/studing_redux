fddb15b7d3d0257f827407c494e4e1118ec23dc5
Guga Villar <grsv.21@gmail.com>
2021-11-05 15:50:36 -0300

    Instalação do redux-saga e do redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
 import { createStore } from 'redux';
 import rootReducers from './modules/rootReducers';
 import { ICartState } from './modules/cart/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export interface IState {
   cart: ICartState
 }
 
-const store = createStore(rootReducers);
+const store = createStore(
+  rootReducers,
+  composeWithDevTools()
+);
 
 export { store }
